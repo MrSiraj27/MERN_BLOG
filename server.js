f30ae45bb4ceef49fe4a253cf0cf9c5a3f65ac42
MrSiraj27 <157973630+MrSiraj27@users.noreply.github.com>
2024-01-28 21:34:32 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const blogRoutes = require("./routes/blogRoutes");
 const app = express();
 
 app.use(cors());
-app.get("/",(req,res)=>{
-  res.send("helo world");
-}
 app.use(express.json());
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/blog", blogRoutes);
